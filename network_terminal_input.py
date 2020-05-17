2869bfda04498439a47ddd8f7387bfbd774cb3aa
ariel ofer <bgzahav8@gmail.com>
2020-05-17 11:33:26 +0300

    added use of NetworkTerminalOutput

--- network_terminal_input.py
+++ network_terminal_input.py
@@ -1,10 +1,12 @@
 from gamebook.game_input import Input
+from network_terminal_output import NetworkTerminalOutput
 
 
 class NetworkTerminalInput(Input):
     def __init__(self, client):
         self.client = client
+        self.output_instance = NetworkTerminalOutput(client)
 
     def input(self, prompt):
-        self.client.send(bytes(prompt, 'utf-8'))
+        self.output_instance.output(bytes(prompt, 'utf-8'))
         return self.client.recv(1024)
