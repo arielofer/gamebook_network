784d709ea0e4e60742f27c901671b0153a3b04d7
ariel ofer <bgzahav8@gmail.com>
2020-05-24 23:58:18 +0300

    added missing functions

--- network_terminal_output.py
+++ network_terminal_output.py
@@ -6,7 +6,11 @@ class NetworkTerminalOutput(Output):
     def __init__(self, client):
         self.client = client
 
-    def output(self, message_type, content):
+    def output(self, message_type="output", content=""):
         message = {"type": message_type, "data": content}
         message_string = json.dumps(message)
         self.client.send(bytes(message_string, 'utf-8'))
+
+    def exit(self, exit_reason):
+        self.output(exit_reason)
+        self.client.close()
